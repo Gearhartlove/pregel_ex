e4ab252662d79251e95073f38c12942dedc641e2
kristoff <kristoffgearhartfinley@gmail.com>
2025-07-05 21:28:57 -0600

    bump

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule PregelEx.MixProject do
   def project do
     [
       app: :pregel_ex,
-      version: "0.1.1",
+      version: "0.1.2",
       elixir: "~> 1.18",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
